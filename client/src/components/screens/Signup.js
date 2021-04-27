2b0efd8695189f6b73db9b0beb651e6afb0e0ec4
Mirza-Hassan <mirzahassan1994@hotmail.com>
2021-04-27 10:11:11 +0500

    Update Signup.js

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -10,12 +10,12 @@ const Signup = ()=>{
 
     const uploadFields = ()=>{
         //Email Validation
-        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(!regex.test(email)){
-            console.log(email);
-            M.toast({html: "invalid email",classes:"#c62828 red darken-3"})
-            return
-        }
+        //const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        //if(!regex.test(email)){
+        //    console.log(email);
+        //    M.toast({html: "invalid email",classes:"#c62828 red darken-3"})
+        //    return
+        //}
         fetch('/signup', {
             method : 'post',
             headers :{ 'Content-Type' : 'application/json'},
@@ -27,10 +27,10 @@ const Signup = ()=>{
         }).then(response => response.json())
           .then(data => {
               if(data.error){
-                M.toast({html: data.error,classes:"#c62828 red darken-3"})
+                //M.toast({html: data.error,classes:"#c62828 red darken-3"})
             }
               else{
-                M.toast({html:data.message,classes:"#43a047 green darken-1"})
+                //M.toast({html:data.message,classes:"#43a047 green darken-1"})
                 history.push('/signin')
             }
         }).catch(err=>{
