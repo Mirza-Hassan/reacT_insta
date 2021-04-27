aeb1c0eca9bdd5af314034fba56003c39fd9c6bd
Mirza-Hassan <mirzahassan1994@hotmail.com>
2021-04-27 10:32:11 +0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -36,7 +36,7 @@ router.post('/signup', (req, res) => {
         res.status(422).json({error:"please add all the fields"})
     }
     //Saving or posting data
-    res.status(200).send({success: "All good!"})
+    //res.status(200).send({success: "All good!"})
     User.findOne({email:email})
     .then((savedUser)=>{
         if(savedUser){
@@ -76,7 +76,7 @@ router.post('/signin', (req, res) =>{
     if(!email || !password){
         return res.status(422).json({error:"please add email or password"})
     }
-    res.status(200).send({success: "All good!"})
+    //res.status(200).send({success: "All good!"})
     User.findOne({email:email})
     .then(savedUser =>{
         if(!savedUser){
